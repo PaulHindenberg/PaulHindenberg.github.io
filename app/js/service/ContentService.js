7039d2a533c81809b2adc6af02a3d93237c3efcd
Paul Hindenberg <paul.hindenberg@gmail.com>
2018-02-13 22:47:10 +0100

    update

--- app/js/service/ContentService.js
+++ app/js/service/ContentService.js
@@ -1,12 +1,109 @@
 /**
- * Created by paulhindenberg on 02.05.14.
+ * Created by paulhindenberg on 13.02.18.
  */
 angular.module('app')
     .factory("ContentService", function () {
         var projects = [
             {
+                employer: 'Gambit Consulting', employerUrl: 'https://www.gambit.de/', employerImgUrl: '', client: 'Novoferm', clientUrl: 'www.novoferm.de/‎',
+                name: 'Integration CRM System C4C', date: '', duration: '9 month', url: '', imgUrl: 'app/img/placeholder.jpg',
+                text: 'During the introduction of the CRM system SAP C4C several bi-directional interfaces were designed and developed for the integration of the SAP ERP system',
+                languages: [
+                    { name: 'ABAP OO' }
+                ],
+                techs: [
+                    { name: 'SAP NetWeaver' },
+                    { name: 'SAP SOA Manager' },
+                ]
+            },
+            {
+                employer: 'Gambit Consulting', employerUrl: 'https://www.gambit.de/', employerImgUrl: '', client: 'Telecommunication (not to be named)', clientUrl: '',
+                name: 'Big Data & Analytics: Analysis of credits', date: '', duration: '6 month', url: '', imgUrl: 'app/img/placeholder.jpg',
+                text: 'Processing of invoice data in the field of mass accounts for the analysis of credits using a Hadoop Big Data solution and QlikSense as an analysis tool.',
+                languages: [
+                    { name: 'Hive QL' },
+                    { name: 'HTML5 CSS3 JS' }
+
+                ],
+                techs: [
+                    { name: 'Amazon EMR' },
+                    { name: 'Hive' },
+                    { name: 'Amazon Lambda' },
+                    { name: 'NodeJS' },
+                    { name: 'AngularJS' }
+
+                ],
+                contents: [
+                    { text: 'Conception and deployment of the Hadoop architecture' },
+                    { text: 'Development of analysis scripts' },
+                    { text: 'Development of interface tools' },
+                    { text: 'Go-Live' },
+                ]
+            },
+            {
+                employer: 'Gambit Consulting', employerUrl: 'https://www.gambit.de/', employerImgUrl: '', client: 'AHG Allgemeine Hospitalgesellschaft', clientUrl: 'https://www.median-kliniken.de/',
+                name: 'Conception and development of an interface landscape - in Project: Rollout of Industry Solution Healthcare (IS-H)', date: '', duration: '9 month', url: '', imgUrl: 'app/img/placeholder.jpg',
+                text: 'Merging of several SAP / ERP systems into a Group-wide deployable SAP IS-H system for 45 clinics and therapy centers. In addition, a system used for years was replaced by a proprietary solution. This solution has a profound integration with SAP ERP on many process levels. For this integration, an interface landscape had to be designed and developed.',
+                contents: [
+                    { text: 'Conception and development of an REST interface landscape in SAP ERP' },
+                    { text: 'Development of various reporting topics in the areas IS-H, FI, CO' }
+                ]
+            },
+            {
+                employer: 'Gambit Consulting', employerUrl: 'https://www.gambit.de/', employerImgUrl: '', client: 'Schmidt + Clemens GmbH & Co. KG', clientUrl: 'https://www.schmidt-clemens.de/',
+                name: 'Reorganization of the ERP process landscape', date: '', duration: '12 month', url: '', imgUrl: 'app/img/placeholder.jpg',
+                text: 'The international operating company carried out a complex restructuring of the production processes including connected interfaces as well as all dependent commercial processes.',
+                contents: [
+                    { text: 'Analysis of legacy systems and their functional description' },
+                    { text: 'Development of various reporting topics in the areas of FI, CO, QM, SCM, SD' },
+                    { text: 'Functional u. Integration tests, go-Live' }
+                ]
+            },
+            {
+                employer: 'Gambit Consulting', employerUrl: 'https://www.gambit.de/', employerImgUrl: '', client: 'Trumpf Medical', clientUrl: 'https://www.trumpfmedical.com/',
+                name: 'Carve Out – Company Code Deletion (SAP LT)', date: '', duration: '6 month', url: '', imgUrl: 'app/img/placeholder.jpg',
+                text: 'The machine tool and laser manufacturer TRUMPF GmbH + Co. KG (Holding) has sold its medical technology division (TRUMPF Medical Systems GmbH + Co. / Trumpf Medical) to the American medical technology manufacturer Hill-Rom Holdings, Inc. To integrate the affected company codes into the new system environment, they were separated from a system copy using the SAP LT scenario "Company code deletion".',
+                languages: [
+                    { name: 'German' },
+                    { name: 'English' }
+
+                ],
+                techs: [
+                    { name: 'SAP Landscape Transformation (LT)' },
+                    { name: 'LT CCD' }
+                ]
+            },
+            {
+                employer: 'Gambit Consulting', employerUrl: 'https://www.gambit.de/', employerImgUrl: '', client: 'DALLI-WERKE GmbH', clientUrl: '',
+                name: 'Ti**a Integration', date: '', duration: '10 month', url: '', imgUrl: 'app/img/placeholder.jpg',
+                text: 'The integration of this plant into Dalli´s IT system landscape included not only complete SAP integration, but also integration of various production subsystems and the complete facility infrastructure.',
+                languages: [
+                    { name: 'ABAP/4', },
+                    { name: 'ABAP OO' }
+
+                ],
+                techs: [
+                    { name: 'SAP ERP 6.0' },
+                    { name: 'SAP NetWeaver' }
+                ]
+            },
+            {
+                employer: 'Gambit Consulting', employerUrl: 'https://www.gambit.de/', employerImgUrl: '', client: 'Novoferm', clientUrl: 'www.novoferm.de/‎',
+                name: 'ongoing SAP development support', date: '', duration: '2.5 years', url: '', imgUrl: 'app/img/placeholder.jpg',
+                text: 'Development of different reports in SAP Modules SD, ,FI, CO',
+                languages: [
+                    { name: 'ABAP/4', },
+                    { name: 'ABAP OO' }
+
+                ],
+                techs: [
+                    { name: 'SAP ERP 6.0' },
+                    { name: 'SAP NetWeaver' }
+                ]
+            },
+            {
                 employer: 'Solid Labs', employerUrl: 'http://www.solid-labs.eu/', employerImgUrl: '', client: '', clientUrl: '',
-                name: 'PocketProf', date: 'since Feb 2014', url: '', imgUrl: 'app/img/placeholder.jpg',
+                name: 'PocketProf', date: 'Feb 2014 - Oct 2014', url: '', imgUrl: 'app/img/placeholder.jpg',
                 text: 'PocketProf is a mobile App for Android and iOS that enables students to consume learning material as well as answering quizzes on the go to get even more educated.',
                 languages: [
                     { name: 'JS,' },
@@ -251,17 +348,18 @@ angular.module('app')
         var me = {
             name: 'Paul Hindenberg', jobTitle: 'SPA Developer & Consultant', email: 'info.hindenberg@gmail.com', imgUrl: 'app/img/mainpic-v2.png', logoUrl: 'app/img/logo_big.png',
             welcomeTxt: "Hello and welcome,\n\n I'm Paul and currently living between between Cologne and Bonn where I'm employed at Gambit Consulting. \n "
-            + "Here I'm working mostly as an ( not only :) ) SAP developer as well as technical Consultant on various projects. "
-            + "Before getting here I was studying Information Management and Consulting (MSc.) in Ludwigshafen am Rhein, University of Applied Science, based in Germany. "
-            + "As Freelancer, I was using my knowledge in different projects, mostly as Application Developer or/and Consultant.\n\n I mostly use this Webpage to keep track of my doings. So if You want to know more details, about me or my work, feel free to have a look.",
+                + "Here I'm working mostly as an ( not only :) ) SAP developer as well as technical Consultant on various projects. "
+                + "Before getting here I was studying Information Management and Consulting (MSc.) in Ludwigshafen am Rhein, University of Applied Science, based in Germany. "
+                + "As Freelancer, I was using my knowledge in different projects, mostly as Application Developer or/and Consultant.\n\n I mostly use this Webpage to keep track of my doings. So if You want to know more details, about me or my work, feel free to have a look.",
             skills: [
-                { text: 'Individual Software Development' },
-                { text: 'Interested in IoT, Big Data, Real Time Applications and other fancy projects' },
-                { text: 'var. ABAP Development' },
-                { text: 'Mass Data Analytics with Hadoop Ecosystem' },
-                { text: 'SPA Development with AngularJS, Vue, NodeJS, Flex, Java, PHP, ..' },
+                { text: 'modern Solution Design' },
+                { text: 'individual Software Development' },
+                { text: 'interested in IoT, Big Data, Real Time Applications and other fancy projects' },
+                { text: 'various ABAP Development' },
+                { text: 'mass Data Analytics with Hadoop Ecosystem' },
+                { text: 'SPA Development with Angular, Vue, NodeJS, Flex, Java, PHP, ..' },
                 { text: 'Multi Media Development with Wowza Media Server' },
-                { text: 'DBs: mySQL, sqlite, mongoDB, redis, SAP HANA' },
+                { text: 'DBs: mySQL, sqlite, mongoDB, Neo4J, ElasticSearch, redis, SAP HANA' },
                 { text: 'Hybrid App Development with Cordova, Ionic or Adobe AIR' },
                 { text: 'Project-Management' },
                 { text: 'Business Processes: (E)EPK, BPMN, KSA' }
@@ -269,7 +367,12 @@ angular.module('app')
         };
         var employments = [
             {
-                employer: 'Gambit Consulting', employerUrl: 'http://www.gambit.de', employerImgUrl: 'https://www.gambit.de/wp-content/uploads/2016/10/gambit_logo.png', date: 'Nov 2014 - today', location: 'Troisdorf, Germany',
+                employer: 'Schoenhofer Sales and Engineering GmbH', employerUrl: 'https://www.schoenhofer.de/', employerImgUrl: 'https://www.schoenhofer.de/fileadmin/templates/img/SSE_Logo_white.png', date: 'Jan 2018 - today', location: 'Siegburg, Germany',
+                job: 'Software Developer and Project Manager', hasProjects: true,
+                descr: ['Working on products and various individual development projects']
+            },
+            {
+                employer: 'Gambit Consulting', employerUrl: 'http://www.gambit.de', employerImgUrl: 'https://www.gambit.de/wp-content/uploads/2016/10/gambit_logo.png', date: 'Nov 2014 - Dec 2017', location: 'Troisdorf, Germany',
                 job: 'IT and SAP Consultant', hasProjects: true,
                 descr: ['Working on various SAP and non SAP projects as technical consultant and developer']
             },
@@ -281,7 +384,7 @@ angular.module('app')
             {
                 employer: 'Self Employed', employerUrl: '', employerImgUrl: '', date: 'since Feb 2012', location: 'Various',
                 job: 'DevCon', hasProjects: true,
-                descr: ['Started being self-employed in Vancouver, BC, Canada', 'Continued it back in Germany next to school for my Master Degree.']
+                descr: ['Started being self-employed in Vancouver, BC, Canada', 'Continued it back in Germany next to school for my master`s degree.']
             },
             {
                 employer: 'Mount Seymour Resorts Ltd', employerUrl: 'https://www.mountseymour.com', employerImgUrl: 'https://www.mountseymour.com/template/images/logos/mount_seymour_logo_01_180x92.png', date: 'Dec 2011 - March 2012', location: 'Vancouver, BC, Canada',
@@ -291,7 +394,7 @@ angular.module('app')
             {
                 employer: 'adesso AG', employerUrl: 'http://www.adesso.de', employerImgUrl: 'http://adesso.de/media/layout_neu/logo/topLogo.gif', date: 'May 2010 - Jul 2011', location: 'Berlin, Germany',
                 job: 'Working Student Development & Consulting', hasProjects: true,
-                descr: ['Primarily worked on Adobe Flex Projects']
+                descr: ['Primarily worked on Adobe Flex projects']
             },
             {
                 employer: 'Fachhochschule Brandenburg', employerUrl: 'http://www.fh-brandenburg.de', employerImgUrl: '', date: 'Oct 2009 - Jan 2010', location: 'Brandenburg, Germany',
@@ -316,9 +419,9 @@ angular.module('app')
         var languages = [{ language: 'German', skill: 'First language' }, { language: 'English', skill: 'Business fluent' }, { language: 'French', skill: 'Basics' }];
 
         var skills = {
-            prog: ['ABAP (Objects)', 'Deepstream (Hub)', 'SAPUI5 / openUI5', 'SAP Fiori', 'oData', 'JSON', 'Apache Hive', 'NodeJS', 'Vue 2 + Vuex', 'HTML', 'CSS', 'JS', 'AngularJS', 'Bootstrap', 'Ionicframework', 'Cordova/Phonegap', 'jQuery', 'SOAP', 'OOP', 'OOAD', 'PHP', 'Java', 'Hibernate', 'SQL', 'Actionscript 3', 'MXML', 'Adobe/Apache Flex (3+4)', 'Adobe AIR', 'Eclipse RCP', 'JFace', 'Coldfusion 10', 'XML'],
-            tools: ['SAP Gateway', 'SAP Cloud Platform', 'SAP Hybris', 'Apache Hadoop', 'mySQL', 'MongoDB', 'Oracle', 'DB-Modelling', 'UML', 'Scrum', 'Jira', 'Hudson/Jenkins CI', 'MS Office', 'Project- & Team Management', 'BPMN', 'KSA', 'Systemanalysis', 'Process Modelling & -Optimisation', 'Mura CMS', 'Typo3'],
-            sap: ['Landscape Transformation', 'SAP NetWeaver', 'ABAP (Objects)', 'SAPUI5 / openUI5', 'SAP Fiori', 'SAP Gateway']
+            prog: ['Docker', 'Docker Compose', 'ABAP', 'SAPUI5 / openUI5', 'SAP Fiori', 'oData', 'Apache Hive', 'NodeJS', 'Vue 2 + Vuex', 'HTML5', 'CSS3', 'JS', 'TS', 'AngularJS', 'Angular2+', 'Material', 'Bootstrap', 'Ionicframework', 'Cordova/Phonegap', 'jQuery', 'REST', 'SOAP', 'OOP', 'OOAD', 'PHP', 'Java', 'Hibernate', 'SQL', 'Actionscript 3', 'MXML', 'Adobe/Apache Flex (3+4)', 'Adobe AIR', 'Eclipse RCP', 'JFace', 'Coldfusion 10', 'XML'],
+            tools: ['Gitlab', 'DeepStream', 'Kong API Gateway', 'Keycloak', 'Apache Hadoop', 'mySQL', 'MongoDB', 'Oracle', 'DB-Modelling', 'UML', 'Scrum', 'Jira', 'Hudson/Jenkins CI', 'MS Office', 'Project- & Team Management', 'BPMN', 'KSA', 'Systemanalysis', 'Process Modelling & -Optimisation', 'Mura CMS', 'Typo3'],
+            sap: ['RICEFW', 'SAP Landscape Transformation (LT)', 'SAP NetWeaver', 'ABAP/4', 'ABAP OO', 'SAPUI5 / openUI5', 'SAP Fiori', 'SAP Gateway', 'SAP Cloud Platform', 'SAP Hybris',]
         };
         var courses = {
             fhb: [
